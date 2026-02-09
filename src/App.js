293986261eadc3705495f39ccd08b5e2f841f074
gfux <ft-2@live.ru>
2026-02-09 21:52:12 +0300

    Switch to HashRouter for reliable SPA routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // App.js
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from 'react-helmet-async';
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
