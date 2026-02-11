8333799576ab666c53acea5cde5c526aa6b55498
gfux <119416035+gfux@users.noreply.github.com>
2026-02-12 00:07:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import ReactDOM from 'react-dom/client'; 
-import { BrowserRouter } from 'react-router-dom'; // ← используем 
-BrowserRouter 
-import App from './App'; 
-import './index.css'; 
-const root = ReactDOM.createRoot(document.getElementById('root')); 
-root.render( 
-<React.StrictMode> 
-<BrowserRouter> 
-<App /> 
-</BrowserRouter> 
-</React.StrictMode> 
-);
+//import ReactDOM from 'react-dom/client'; 
+//import { BrowserRouter } from 'react-router-dom'; // ← используем 
+//BrowserRouter 
+//import App from './App'; 
+//import './index.css'; 
+//const root = ReactDOM.createRoot(document.getElementById('root')); 
+//root.render( 
+//<React.StrictMode> 
+//<BrowserRouter> 
+//<App /> 
+//</BrowserRouter> 
+//</React.StrictMode> 
+//);
+
+
+
+
+import React from 'react';  // React всегда первый!
+import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom'; // HashRouter вместо BrowserRouter
+import './index.css';
+import App from './App';
